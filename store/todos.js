a0be33130f90e816d9957c871d1a469d586be875
Laura Santamaria <laura.santamaria@rackspace.com>
2018-10-22 13:40:52 -0500

    feat(vuex): starting to work on the store

---
+++ store/todos.js
@@ -0,0 +1,20 @@
+// jshint esversion:6
+
+export const state = () => ({
+  list: []
+});
+
+export const mutations = {
+  add (state, text) {
+    state.list.push({
+      text: text,
+      done: false
+    });
+  },
+  remove (state, { todo }) {
+    state.list.splice(state.list.indexOf(todo), 1);
+  },
+  toggle (state, todo) {
+    todo.done = !todo.done;
+  }
+};
